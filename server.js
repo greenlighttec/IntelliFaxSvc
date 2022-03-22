000a9959af73c65528f7092e1c2a32c23990834e
Greenlighttec <mendy@mendyonline.com>
2022-03-22 19:50:11 -0400

    Add BasicAuth as required

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const url = require('url');
 const path = require('path');
 const telnyx = require('telnyx');
 const body = require('body-parser');
+const basicAuth = require('express-basic-auth');
 
 // load required internal modules
 const tools = require('./lib/tools.js');
 const faxstatus = require('./lib/ReportDeliveryStatus.js');
-
+const AuthUsers =  require('./unused/secrets.json');
 const host = '0.0.0.0';
 const port = 8340;
 
+// testing variable stuff ignore me please A.S.
+var a = 5, b = 2
 
 app.get('/AccountProvisioningDetail/:deviceMac', (req,res) => {
 
@@ -32,31 +35,24 @@ app.get('/AccountProvisioningDetail/:deviceMac', (req,res) => {
 
 });
 
-app.get('/AuthenticateAccount', (req,res) => {
+app.get('/AuthenticateAccount', basicAuth({users: AuthUsers, challenge: true,ream: 'faxapp.hostedpbx.intellicloud.tech' }) ,(req,res) => {
 
                 console.log('Heres the URL being hit in Account Authentication');
-                console.log(urlObject);
+                console.log(req.path);
 		var answer = tools.multiply(a,b);
-                var httpresponse = `<h1>${paths[0]}</h1><p>Hooray you made it, your answer is ${answer}</p>`;
+                var httpresponse = `<h1>AuthenticateAccount</h1><p>Hooray you made it, your answer is ${answer}</p>`;
+		console.log(AuthUsers);
 		res.send(httpresponse)
 
 });
 
 app.post('/DeliverImage', (req, res) => {
+
 		//Incoming Function only to send fax to ATA
 		var answer = tools.multiply(a,b);
-                var httpresponse = `<h1>${paths[0]}</h1><p>Hooray you made it, your answer is ${answer}</p>`;
+                var httpresponse = `<h1>Deliver Image</h1><p>Hooray you made it, your answer is ${answer}</p>`;
 
-		if (req.method == 'POST') {
-			let data = ''
-			req.on('data', chunk => {
-				data += chunk;
-			})
-			req.on('end', () => {console.log(data);})
-
-			httpresponse = httpresponse + '<h3>Data was posted</h3><p> ' + ' </p>'
-		}
-
+		console.log(req)
 		res.send(httpresponse)
 
 });
