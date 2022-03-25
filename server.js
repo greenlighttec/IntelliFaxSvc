86ffcd765070388be523fb1cf0cead6b7ddd84ae
Greenlighttec <mendy@mendyonline.com>
2022-03-24 23:19:02 -0400

    Added additional functionality

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 // load required external modules
 const express =  require('express');
 const app  = express();
-const fs = require('fs').promises;
-const url = require('url');
 const path = require('path');
 const telnyx = require('telnyx');
 const body = require('body-parser');
@@ -11,8 +9,6 @@ const basicAuth = require('express-basic-auth');
 // load required internal modules
 const tools = require('./lib/tools.js');
 const faxstatus = require('./lib/ReportDeliveryStatus.js');
-//const AuthUsers =  require('./secrets/creds.json');
-//const DeviceUserMapping = require('./secrets/devicemapping.json');
 const authenticateUsers = require('./lib/AuthenticateAccount.js');
 const host = '0.0.0.0';
 const port = 8340;
@@ -20,25 +16,22 @@ const port = 8340;
 // testing variable stuff ignore me please A.S.
 var a = 5, b = 2
 
+app.get('/AccountProvisioningDetail', (req,res) => {
+	res.send('<html><head><title>404 - Page not found</title></head><body><h1>404 - Page not found</h1></body></html>');
+
+});
+
 app.get('/AccountProvisioningDetail/:deviceMac', (req,res) => {
 
-		const deviceMac = req.params.deviceMac
-		if (deviceMac) {
-			console.log('Provisioning request for MAC ' + deviceMac)
-			res.contentType('application/xml');
-			res.sendFile(path.join(__dirname , '/public/' + deviceMac + '.xml' ));
-		}
-		else {
-			var answer = tools.multiply(a,b);
-			var httpresponse = `<h1>${paths[0]}</h1><p>Hooray you made it, your answer is ${answer}</p>`;
-			res.send(httpresponse)
-
-		}
+	const deviceMac = req.params.deviceMac;
+	console.log('Provisioning request for MAC ' + deviceMac)
+	res.contentType('application/xml');
+	res.sendFile(path.join(__dirname , '/public/' + deviceMac + '.xml' ));
 
 });
 
 
-app.get('/AuthenticateAccount', authenticateUsers, (req, res) => {
+app.get('/AccountLoginDetail', authenticateUsers, (req, res) => {
 
 	console.log(res.locals.deviceMac)
 	res.contentType('application/xml');
@@ -46,14 +39,11 @@ app.get('/AuthenticateAccount', authenticateUsers, (req, res) => {
 
 });
 
-app.post('/DeliverImage', (req, res) => {
+app.post('/ReceiveFax', (req, res) => {
 
-		//Incoming Function only to send fax to ATA
-		var answer = tools.multiply(a,b);
-                var httpresponse = `<h1>Deliver Image</h1><p>Hooray you made it, your answer is ${answer}</p>`;
-
+		//Incoming Function from Telnyx and  send fax to ATA to be printed by the fax machine.
+		//Send API calls to https://rest-api.faxback.com/nsps/nsps.aspx?target=atamime
 		console.log(req)
-		res.send(httpresponse)
 
 });
 
