ac2b5a6bfc60e736f887879d184336e753bd6abb
Mendy <greenlighttec@users.noreply.github.com>
2022-11-30 18:13:10 -0500

    Update init.js

--- init.js
+++ init.js
@@ -3,5 +3,5 @@ models.sequelize.sync()
 const Clients = require(process.env.WORKDIR + 'models').Clients;
 const users = require(process.env.WORKDIR + 'lib/admin/UserAccounts.js');
 
-Clients.create({id: 1,clientname: 'Default Site'})
+Clients.create({id: 1,clientname: process.env.ADMIN_COMPANY})
 users.create(1, 'Default', 'Admin', process.env.ADMIN_USER, process.env.ADMIN_PASS, user => console.log(user))
