3424ba3872c772702358ae722d7ba3bf3320005e
greenlighttec <mendy@mendyonline.com>
2022-11-30 05:31:29 +0000

    Update init.js file.

--- init.js
+++ init.js
@@ -1,2 +1,4 @@
+var models = require('./models')
+models.sequelize.sync()
 const users = require(process.env.WORKDIR + 'lib/admin/UserAccounts.js')
 users.create(0, 'Default', 'Admin', 'Admin', 'Admin', user => console.log(user))
