3e1e4222473a82a8d884cf1f5555604c4cae51ce
greenlighttec <mendy@mendyonline.com>
2022-11-30 06:17:05 +0000

    Modified init.js

--- init.js
+++ init.js
@@ -1,4 +1,15 @@
-var models = require('./models')
+var models = require(process.env.WORKDIR + 'models')
 models.sequelize.sync()
-const users = require(process.env.WORKDIR + 'lib/admin/UserAccounts.js')
-users.create(0, 'Default', 'Admin', 'Admin', 'Admin', user => console.log(user))
+const Clients = require(process.env.WORKDIR + 'models').Clients;
+const Users = require(process.env.WORKDIR + 'models').Users;
+
+Clients.create({id: -1,clientname: 'Default Site'})
+
+Users.create({
+    firstname: 'Default',
+    lastname: 'Admin',
+    username: 'admin',
+    password: 'admin',
+    clientid: -1
+})
+//users.create(-1, 'Default', 'Admin', 'Admin', 'Admin', user => console.log(user))
