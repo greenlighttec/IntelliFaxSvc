1ba4f6586cce65b3e93912bcb233b56f4fadf3a5
Mendy <greenlighttec@users.noreply.github.com>
2022-11-30 17:56:33 -0500

    Update init.js

--- init.js
+++ init.js
@@ -3,13 +3,14 @@ models.sequelize.sync()
 const Clients = require(process.env.WORKDIR + 'models').Clients;
 const Users = require(process.env.WORKDIR + 'models').Users;
 
-Clients.create({id: -1,clientname: 'Default Site'})
+Clients.create({id: 1,clientname: 'Default Site'})
 
-Users.create({
+/* Users.create({
     firstname: 'Default',
     lastname: 'Admin',
     username: 'admin',
     password: 'admin',
     clientid: -1
-})
-//users.create(-1, 'Default', 'Admin', 'Admin', 'Admin', user => console.log(user))
+}) */
+
+users.create(1, 'Default', 'Admin', 'Admin', 'Admin', user => console.log(user))
