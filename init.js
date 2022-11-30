01a3f78408bb7739995d67866098892d3abc6cd0
Mendy <greenlighttec@users.noreply.github.com>
2022-11-30 18:11:19 -0500

    Update init.js

--- init.js
+++ init.js
@@ -4,13 +4,4 @@ const Clients = require(process.env.WORKDIR + 'models').Clients;
 const users = require(process.env.WORKDIR + 'lib/admin/UserAccounts.js');
 
 Clients.create({id: 1,clientname: 'Default Site'})
-
-/* Users.create({
-    firstname: 'Default',
-    lastname: 'Admin',
-    username: 'admin',
-    password: 'admin',
-    clientid: -1
-}) */
-
-users.create(1, 'Default', 'Admin', 'Admin', 'Admin', user => console.log(user))
+users.create(1, 'Default', 'Admin', process.env.ADMIN_USER, process.env.ADMIN_PASS, user => console.log(user))
