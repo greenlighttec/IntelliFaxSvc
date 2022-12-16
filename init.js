d4bf4155d2e0ee85cdef84ad12fdd9180d09c183
Mendy <greenlighttec@users.noreply.github.com>
2022-12-15 22:03:48 -0500

    Update init.js

--- init.js
+++ init.js
@@ -5,4 +5,3 @@ const users = require(process.env.WORKDIR + 'lib/admin/UserAccounts.js');
 
 var createClients = await Clients.create({id: 1,clientname: process.env.ADMIN_COMPANY})
 var createUsers = await users.create(1, 'Default', 'Admin', process.env.ADMIN_USER, process.env.ADMIN_PASS, user => console.log(user))
-var name = 'hello'
